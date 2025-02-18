b4fbe0bfbaf378cdffb9ae48a478cc7f4bd9dd7b
Radomir828 <radsobic@yandex.com>
2025-02-18 14:14:48 +0400

    feat: исходный список дел выводится на страницу(JS)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-import "./styles/styles.css"
+import "./styles/styles.css";
+
+import { todos } from "./utils/constants";
+
+const contentElement = document.querySelector(".todos__list");
+const template = document.querySelector(
+  "#todo-item-template"
+) as HTMLTemplateElement;
+
+const createItem = (name: string) => {
+  const itemElement = template.content
+    .querySelector(".todo-item")
+    .cloneNode(true) as HTMLElement;
+
+  const itemTitle = itemElement.querySelector(".todo-item__text");
+  itemTitle.textContent = name;
+
+  return itemElement;
+};
+
+todos.forEach((item) => {
+  const itemElement = createItem(item);
+  contentElement.prepend(itemElement);
+});
